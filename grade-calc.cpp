1d563832308a72a7858c4a014fcdebbf28f292cc
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-07-03 21:00:45 -0700

    added welcome messages

--- grade-calc.cpp
+++ grade-calc.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
 int main(){
-
+    std::cout << "--- Welcome to my grade calculator! ---" << std::endl;
+    std::cout << "Enter your grade, followed by its weight." << std::endl;
+    std::cout << "Enter any non-number to continue." << std::endl;
 }
