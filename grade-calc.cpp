db9147ca9146c8123da23b32cf3c966f1a461262
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-07-03 21:05:54 -0700

    input for grade works

--- grade-calc.cpp
+++ grade-calc.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
-int main(){
-    std::cout << "--- Welcome to my grade calculator! ---" << std::endl;
-    std::cout << "Enter your grade, followed by its weight." << std::endl;
-    std::cout << "Enter any non-number to continue." << std::endl;
+int main()
+{
+    std::cout << std::endl
+              << "--- Welcome to my grade calculator! ---" << std::endl;
+    std::cout << std::endl
+              << "Enter your grade, followed by its weight." << std::endl;
+    std::cout << "Enter any non-number to continue." << std::endl
+              << std::endl;
+
+    double grade, percentage, totalGrade, totalPercent;
+    while (1)
+    {
+        std::cout << "Enter your assignment grade: ";
+        std::cin >> grade;
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(32767, '\n');
+            break;
+        }
+    }
 }
