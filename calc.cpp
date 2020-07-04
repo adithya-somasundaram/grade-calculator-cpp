3fe7d2f359325cfd3e3e9112a7399d577fe10fe7
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-07-03 22:13:18 -0700

    renaming vars

--- calc.cpp
+++ calc.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-void remainingGrades(float grade, float percent)
+void remainingGrades(float current_grade, float weight)
 {
-    float percent_left = 100 - percent;
+    float percent_left = 100 - weight;
     std::string grades[] = {"A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F"};
     float gradeNums[] = {97, 93, 90, 87, 83, 80, 77, 73, 70, 67, 63, 60};
     float score;
@@ -14,12 +14,12 @@ void remainingGrades(float grade, float percent)
     {
         if (i == 12)
         {
-            score = 100 * (60 - grade) / percent_left;
+            score = 100 * (60 - current_grade) / percent_left;
             std::cout << "< 60% (" << grades[i] << "): <" << score << std::endl;
         }
         else
         {
-            score = 100 * (gradeNums[i] - grade) / percent_left;
+            score = 100 * (gradeNums[i] - current_grade) / percent_left;
             std::cout << gradeNums[i] << "% (" << grades[i] << "): " << score << std::endl;
         }
     }
