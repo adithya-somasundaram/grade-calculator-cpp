bf665d13d8ab57e1ef6760f2ae6c07e59ba4a33a
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-07-03 22:16:37 -0700

    added comments

--- grade-calc.cpp
+++ grade-calc.cpp
@@ -10,12 +10,15 @@ int main()
               << "Enter your assignment grade, followed by its weight." << std::endl;
     std::cout << "Enter any non-number to continue." << std::endl;
 
+    // will store user inputs, and total grade/weight
     float grade, percentage, totalGrade = {0}, totalPercent = {0};
     while (1)
     {
+        // grade input
         std::cout << std::endl
                   << "Enter your assignment grade: ";
         std::cin >> grade;
+        // break out of loop if input is non-float
         if (std::cin.fail())
         {
             std::cin.clear();
@@ -23,20 +26,25 @@ int main()
             break;
         }
 
+        // weight input
         std::cout << "Enter the weight (percentage) of the grade: ";
         std::cin >> percentage;
+        // break out of loop if input is non-float
         if (std::cin.fail())
         {
             std::cin.clear();
             std::cin.ignore(32767, '\n');
             break;
         }
+
+        // increment total weight and check that its less than 100
         totalPercent += percentage;
         if (percentage > 100)
         {
             std::cout << "Went over 100% :(" << std::endl;
             return EXIT_FAILURE;
         }
+        // increment total grade
         totalGrade += (grade * (percentage / 100));
     }
     remainingGrades(totalGrade, totalPercent);
